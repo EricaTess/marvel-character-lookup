9f9bf83f7e55a72c68b73f541c9ada9664003b4f
EricaTess <sanchez.ericat@gmail.com>
2020-08-28 20:59:06 -0700

    cleaned code and removed comments, updated deadpool image to show only when no char is found

--- src/components/CharacterCards.js
+++ src/components/CharacterCards.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
-// import '../public/styles/CharacterCards.css';
 
 export default class CharacterCards extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showDescription: false
+    constructor() {
+        super();
+        this.state = {
+            showDescription: false
+        }
     }
-  }
 
-  render() {
+    render() {
 
-    return(
-        <Card className='cards' bg={'dark'} text={'light'} style={{ width: '10rem' }}>
-          <Card.Img variant="top" src={this.props.img.path + '.' + this.props.img.extension} />
-          <Card.Body
-            id={this.props.id}
-            onMouseEnter={() => this.setState({showDescription: true})}
-            onMouseLeave={() => this.setState({showDescription: false})}>
-            <Card.Title>{this.props.characters.name}</Card.Title>
-            {/* /* Character Info to add later */}
-            <Card.Text>
-              <div className='char-description'>{this.state.showDescription && (this.props.characters.description)}</div>
-            </Card.Text>
-          </Card.Body>
-        </Card>    
-    )
-  }
+        return(
+            <Card className='cards' bg={'dark'} text={'light'} style={{ width: '10rem' }}>
+                <Card.Img variant="top" src={this.props.img.path + '.' + this.props.img.extension} />
+                <Card.Body id={this.props.id}
+                    onMouseEnter={() => this.setState({showDescription: true})}
+                    onMouseLeave={() => this.setState({showDescription: false})}>
+                    <Card.Title>{this.props.characters.name}</Card.Title>
+                    <Card.Text>
+                        <div className='char-description'>{this.state.showDescription && (this.props.characters.description)}</div>
+                    </Card.Text>
+                </Card.Body>
+            </Card>    
+        )
+    }
 }
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,67 +4,71 @@ import deadpool from './deadpool.png'
 import CharacterCards from './CharacterCards'
 
 export default class Characters extends Component {
-  constructor () {
-      super();
-      this.state = {
+    constructor () {
+        super();
+        this.state = {
         characters: [],
         query: ''
-      }
-  }
-
-  componentDidMount = () => {
-      //Get 20 characters from Marvel API
-      fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=' + process.env.REACT_APP_API_KEY + this.state.query)
-        .then(res =>  res.json())
-        .then(data => this.setState({
-            characters: data.data.results
-        },
-        console.log(data.data.results)))
-        
-  }
-
-  //Set user input (character name) to state
-  handleTextInput = (e) => {
-      //if no character name added, show all characters
-      if (e.target.value === '') {
-          this.setState({
-              query: ''
-          })
-      } else {
-        this.setState({
-            query: '&name=' + e.target.value
-        })
-      }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-
-    //Add Marvel Character input to API Request
-    fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=' + process.env.REACT_APP_API_KEY + this.state.query)
-        .then(res =>  res.json())
-        .then(data => this.setState({
-            characters: data.data.results
-        },
-        console.log(data.data.results)))
-        .catch(err => {
-            console.log('Error fetching and parsing data', err)
-        })
-  }
-
-  render() {
-
-    //Add results from API Request to Character Card components
-    const validCharacter = this.state.characters;
-    let showCharacters;
-    let noCharacters;
-    //If fetch result is empty, return a message
-    if (validCharacter.length !== 0) {
-        showCharacters = this.state.characters.map(char => {
-            return <CharacterCards key={char.id} id={char.id} characters={char} img={char.thumbnail}/>})
-    } else {
-        noCharacters = <p>Move along, Nothing to see here</p>
+        }
     }
+
+    componentDidMount = () => {
+        //Get 20 characters from Marvel API
+        fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=' + process.env.REACT_APP_API_KEY + this.state.query)
+        .then(res =>  res.json())
+        .then(data => this.setState({
+            characters: data.data.results
+        },
+        console.log(data.data.results)))
+            
+    }
+
+    //Set user input (character name) to state
+    handleTextInput = (e) => {
+        //if no character name added, show all characters
+        if (e.target.value === '') {
+            this.setState({
+                query: ''
+            })
+        } else {
+            this.setState({
+                query: '&name=' + e.target.value
+            })
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        //Add Marvel Character input to API Request
+        fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=' + process.env.REACT_APP_API_KEY + this.state.query)
+            .then(res =>  res.json())
+            .then(data => this.setState({
+                characters: data.data.results
+            },
+            console.log(data.data.results)))
+            .catch(err => {
+                console.log('Error fetching and parsing data', err)
+            })
+    }
+
+    render() {
+
+        //Add results from API Request to Character Card components
+        const validCharacter = this.state.characters;
+        let showCharacters;
+        let noCharacters;
+        //If fetch result is empty, return a message
+        if (validCharacter.length !== 0) {
+            showCharacters = this.state.characters.map(char => {
+                return <CharacterCards key={char.id} id={char.id} characters={char} img={char.thumbnail}/>})
+        } else {
+            noCharacters = 
+            (<div className='oops-message'>
+                <p>Move along, Nothing to see here</p>
+                <img className='oops-deadpool' src={deadpool} />
+            </div>)
+        }
     
     return (
         <React.Fragment>
@@ -86,13 +90,10 @@ export default class Characters extends Component {
                     <CardColumns style={{justifyContent: 'center', display: 'contents'}}>
                         {showCharacters}
                     </CardColumns>
-                    <div className='oops-message'>
-                      {noCharacters}
-                      <img className='oops-deadpool' src={deadpool} />
-                    </div>
+                        {noCharacters}
                 </Row>
             </Container>
         </React.Fragment>
-      );
-  }
+        );
+    }
 }
