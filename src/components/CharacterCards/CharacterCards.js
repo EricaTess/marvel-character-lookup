e422faa4148276621d647aa1a01a56bd99994c2a
EricaTess <sanchez.ericat@gmail.com>
2020-08-31 20:40:54 -0700

    added popover to display character description

--- src/components/CharacterCards/CharacterCards.js
+++ src/components/CharacterCards/CharacterCards.js
@@ -12,24 +12,33 @@ export default class CharacterCards extends Component {
         }
     }
 
+    showCharInfo = () => {
+        if (this.props.characters.description !== '') {
+            return  (<Popover id="popover">
+                        <Popover.Content className='char-description'>
+                            {this.state.showDescription && (this.props.characters.description)}
+                        </Popover.Content>
+                    </Popover>)
+        } else {
+            return (<Popover id="popover">
+                        <Popover.Content className='char-description'>
+                            No Description Available
+                        </Popover.Content>
+                    </Popover>)
+        }
+    }
+
     render() {
 
-        const charInfo =    <Popover id="popover">
-                                <Popover.Content className='char-description'>
-                                    {this.state.showDescription && (this.props.characters.description)}
-                                </Popover.Content>
-                            </Popover>
-
         return(
-            <Card className='cards' bg={'dark'} text={'light'} style={{ width: '10rem' }}>
+            <Card className='cards' bg={'dark'} text={'light'} style={{ width: '10rem' }}
+                    onMouseEnter={() => this.setState({showDescription: true})}
+                    onMouseLeave={() => this.setState({showDescription: false})}>
                 <Card.Img variant="top" src={this.props.img.path + '.' + this.props.img.extension} />
                 <OverlayTrigger placement="bottom"
                     delay={{ show: 250, hide: 400 }}
-                    overlay={charInfo}
-                >
-                    <Card.Body id={this.props.id}
-                            onMouseEnter={() => this.setState({showDescription: true})}
-                            onMouseLeave={() => this.setState({showDescription: false})}>
+                    overlay={this.showCharInfo()}>
+                    <Card.Body id={this.props.id}>
                         <Card.Title>{this.props.characters.name}</Card.Title>
                         <Card.Text>
                         </Card.Text>
